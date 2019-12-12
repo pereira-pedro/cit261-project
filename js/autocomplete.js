2e55aaa5d1e6db6b2ab1aa2a44b3fdaef7a3d203
Pedro Bastos <pedro@pontonet.com>
2019-12-11 23:30:27 -0400

    Custom autocomplete.

---
+++ js/autocomplete.js
@@ -0,0 +1,163 @@
+class AutoComplete {
+  constructor(target, progress, selectHandler) {
+    this._target = document.getElementById(target);
+    this._progressBar = document.getElementById(progress);
+    this._selectHandler = selectHandler;
+
+    this._target.addEventListener("input", this._inputHandler);
+    this._target.addEventListener("keydown", this._keydownHandler);
+    this._target.addEventListener("focus", this._focusHandler);
+
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", e => {
+      this._closeAllLists(e.target);
+    });
+  }
+
+  _inputHandler = e => {
+    var val = e.target.value;
+
+    /*close any already open lists of autocompleted values*/
+    this._closeAllLists();
+    if (!val || val.length < 3) {
+      return false;
+    }
+
+    this._currentFocus = -1;
+
+    this._progressBar.style.display = "block";
+
+    _apiGet(
+      "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/auto-complete",
+      {
+        query: val
+      },
+      response => {
+        this._createList(response.ResultSet.Result);
+      },
+      error => {
+        showDialogMessage(error);
+      },
+      () => {
+        this._progressBar.style.display = "none";
+      }
+    );
+  };
+
+  _keydownHandler = e => {
+    var element = document.getElementById(e.target.id + "autocomplete-list");
+    if (element) {
+      element = element.getElementsByTagName("div");
+    }
+    if (e.keyCode == 40) {
+      /*If the arrow DOWN key is pressed,
+            increase the currentFocus variable:*/
+      this._currentFocus++;
+      /*and and make the current item more visible:*/
+      this._addActive(element);
+    } else if (e.keyCode == 38) {
+      //up
+      /*If the arrow UP key is pressed,
+            decrease the currentFocus variable:*/
+      this._currentFocus--;
+      /*and and make the current item more visible:*/
+      this._addActive(element);
+    } else if (e.keyCode == 13) {
+      /*If the ENTER key is pressed, prevent the form from being submitted,*/
+      e.preventDefault();
+      if (this._currentFocus > -1) {
+        /*and simulate a click on the "active" item:*/
+        if (element) {
+          element[this._currentFocus].click();
+        }
+      }
+    }
+  };
+
+  _focusHandler = e => {
+    /*close any already open lists of autocompleted values*/
+    this._closeAllLists();
+
+    this._currentFocus = -1;
+
+    var items = localStorage.getItem("company_list");
+    if (items === null) {
+      return;
+    }
+
+    this._createList(JSON.parse(items));
+  };
+
+  _createList = elements => {
+    var a, b;
+
+    /*create a DIV element that will contain the items (values):*/
+    a = document.createElement("DIV");
+    a.setAttribute("id", this._target.id + "autocomplete-list");
+    a.setAttribute("class", "autocomplete-items");
+
+    /*append the DIV element as a child of the autocomplete container:*/
+    this._target.parentNode.appendChild(a);
+
+    elements.forEach(row => {
+      b = document.createElement("DIV");
+      b.setAttribute("data-symbol", row.symbol);
+      b.setAttribute("data-name", row.name);
+      b.innerHTML = `${row.symbol} - ${row.name}`;
+
+      b.addEventListener("click", e => {
+        this._target.setAttribute("data-symbol", e.target.dataset.symbol);
+        this._target.setAttribute("data-name", e.target.dataset.name);
+        this._closeAllLists();
+
+        this._selectHandler();
+      });
+      a.appendChild(b);
+    });
+  };
+  _addActive = elements => {
+    /*a function to classify an item as "active":*/
+    if (!elements) {
+      return false;
+    }
+
+    /*start by removing the "active" class on all items:*/
+    this._removeActive(elements);
+
+    if (this._currentFocus >= elements.length) {
+      this._currentFocus = 0;
+    }
+
+    if (this._currentFocus < 0) {
+      this._currentFocus = elements.length - 1;
+    }
+
+    /*add class "autocomplete-active":*/
+    elements[this._currentFocus].classList.add("autocomplete-active");
+  };
+
+  _removeActive = elements => {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    elements.forEach(row => {
+      row.classList.remove("autocomplete-active");
+    });
+  };
+
+  /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+  _closeAllLists = element => {
+    if (element === undefined) {
+      return;
+    }
+    const items = document.getElementsByClassName("autocomplete-items");
+
+    if (items.length === 0) {
+      return;
+    }
+    for (var i = 0; i < items.length; i++) {
+      if (element != items[i] && element != this._target) {
+        items[i].parentNode.removeChild(items[i]);
+      }
+    }
+  };
+}
--- js/main.js
+++ js/main.js
@@ -1,286 +1,182 @@
 const _api = {
-    host: "apidojo-yahoo-finance-v1.p.rapidapi.com",
-    key: "354a251140mshf27709a6ed368a8p109c2cjsn341179353f81",
-    lang: 'en',
-    region: 'US'
+  host: "apidojo-yahoo-finance-v1.p.rapidapi.com",
+  key: "354a251140mshf27709a6ed368a8p109c2cjsn341179353f81",
+  lang: "en",
+  region: "US"
 };
-
-const _intervals = [
-    { name: '1 minute', value: '1m' },
-    { name: '2 minutes', value: '2m' },
-    { name: '5 minutes', value: '5m' },
-    { name: '15 minutes', value: '15m' },
-    { name: '1 hour', value: '60m' },
-    { name: '1 day', value: '1d' }
-];
-
-const _ranges = [
-    { name: '1 day', value: '1d' },
-    { name: '5 days', value: '5d' },
-    { name: '1 month', value: '1mo' },
-    { name: '3 months', value: '3mo' },
-    { name: '6 months', value: '6mo' },
-    { name: '1 year', value: '1y' },
-    { name: '2 years', value: '2y' },
-    { name: '5 years', value: '5y' },
-    { name: '10 years', value: '10y' }
-];
 
 var _stockChart = null;
 
-const onDOMLoaded = function () {
-    const companySelector = document.getElementById('company-selector');
+const onDOMLoaded = () => {
+  const companySelector = document.getElementById("company-selector");
 
-    companySelector.addEventListener('input', companyAutocomplete);
-    companySelector.addEventListener('focus', companyList)
-    companySelector.addEventListener('change', companySelected)
-    window.addEventListener("resize", () => {
-        var event = new Event('change');
-        companySelector.dispatchEvent(event);
-    });
+  window.addEventListener("resize", () => {
+    var event = new Event("change");
+    companySelector.dispatchEvent(event);
+  });
+
+  new AutoComplete("company-selector", "progress-bar", selectCompany);
 };
 
 if (
-    document.readyState === "complete" ||
-    (document.readyState !== "loading" && !document.documentElement.doScroll)
+  document.readyState === "complete" ||
+  (document.readyState !== "loading" && !document.documentElement.doScroll)
 ) {
-    onDOMLoaded();
+  onDOMLoaded();
 } else {
-    document.addEventListener("DOMContentLoaded", onDOMLoaded);
+  document.addEventListener("DOMContentLoaded", onDOMLoaded);
 }
 
-function companyAutocomplete(e) {
-    const query = e.target.value;
+selectCompany = () => {
+  const company = document.getElementById("company-selector");
 
-    // this is valid only if the user is typing
-    /*if (!('inputType' in e)) {
+  if (!("symbol" in company.dataset) || company.dataset.symbol.length === 0) {
+    return;
+  }
+  document.getElementById("company-name").innerText = company.dataset.name;
+  document.getElementById("company-header").style.display = "flex";
+
+  drawCompanyChart(company.dataset.symbol);
+};
+
+drawCompanyChart = company => {
+  const stockChart = document.getElementById("stock-chart");
+  const progressBar = document.getElementById("progress-spinner");
+  const message = document.getElementById("message");
+
+  progressBar.style.display = "block";
+  stockChart.style.display = "none";
+  message.innerHTML = "";
+
+  if (_stockChart !== null) {
+    _stockChart.clearChart();
+  }
+
+  _apiGet(
+    "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-chart",
+    {
+      interval: document.getElementById("interval").value,
+      symbol: company,
+      range: document.getElementById("range").value
+    },
+    response => {
+      if (!("timestamp" in response.chart.result[0])) {
+        message.innerHTML = "<i>There's no data to show.</i>";
         return;
-    }*/
-
-    // only queries with 3 characters
-    if (query.length < 3) {
-        return;
+      }
+      google.charts.load("current", { packages: ["corechart"] });
+      google.charts.setOnLoadCallback(() => _drawChart(response));
+    },
+    error => {
+      showDialogMessage(error);
+    },
+    () => {
+      stockChart.style.display = "block";
+      progressBar.style.display = "none";
     }
+  );
+};
 
-    const datalist = new DataList('company-list');
-    datalist.empty();
+_apiGet = (uri, query, handlerThen, handlerCatch, handleFinally) => {
+  const url = new URL(uri);
 
-    const progressBar = document.getElementById('progress-bar');
-    progressBar.style.display = 'block';
+  query.lang = _api.lang;
+  query.region = _api.region;
 
-    _apiGet(
-        'https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/auto-complete',
-        {
-            query: query
-        },
-        response => {
-            const datalist = new DataList('company-list');
-            datalist.empty();
-            datalist.addElement(response.ResultSet.Result.map(row => {
-                return {
-                    value: row.symbol,
-                    text: row.name
-                };
-            }));
-        },
-        error => {
-            console.log(JSON.stringify(error));
-        },
-        () => {
-            progressBar.style.display = 'none';
-        });
-}
+  url.search = new URLSearchParams(query).toString();
 
-function companyList(e) {
-    var items;
-
-    if ((items = localStorage.getItem("company_list")) === null) {
-        return;
+  fetch(url, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": _api.host,
+      "x-rapidapi-key": _api.key
     }
-    const datalist = new DataList('company-list');
-    const companyList = JSON.parse(items);
-
-    datalist.empty();
-    datalist.addElement(companyList);
-
-    var keyboardEvent = document.createEvent("KeyboardEvent");
-    var initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? "initKeyboardEvent" : "initKeyEvent";
-
-    keyboardEvent[initMethod](
-        "keypress", // event type : keydown, keyup, keypress
-        true, // bubbles
-        true, // cancelable
-        window, // viewArg: should be window
-        false, // ctrlKeyArg
-        false, // altKeyArg
-        false, // shiftKeyArg
-        false, // metaKeyArg
-        40, // keyCodeArg : unsigned long the virtual key code, else 0
-        0 // charCodeArgs : unsigned long the Unicode character associated with the depressed key, else 0
-    );
-    e.target.dispatchEvent(keyboardEvent);
-}
-
-function companySelected(e) {
-    const company = document.querySelectorAll(`option[value='${e.target.value}']`);
-
-    if (company[0] === null) {
-        return;
-    }
-    document.getElementById("company-name").innerText = company[0].innerText;
-    document.getElementById("company-header").style.display = "flex";
-
-    drawCompanyChart(company[0].value);
-}
-
-function drawCompanyChart(company) {
-
-    if (company === null) {
-        return;
-    }
-
-    const stockChart = document.getElementById('stock-chart');
-    const progressBar = document.getElementById('progress-spinner');
-    const message = document.getElementById("message");
-
-    progressBar.style.display = 'block';
-    stockChart.style.display = 'none';
-    message.innerHTML = '';
-
-    if (_stockChart !== null) {
-        _stockChart.clearChart();
-    }
-
-    _apiGet(
-        "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-chart",
-        {
-            interval: document.getElementById("interval-data").value,
-            symbol: company,
-            range: document.getElementById("range-data").value
-        },
-        response => {
-            if (!('timestamp' in response.chart.result[0])) {
-                message.innerHTML = "<i>There's no data to show.</i>";
-                return;
-            }
-            google.charts.load('current', { 'packages': ['corechart'] });
-            google.charts.setOnLoadCallback(() => _drawChart(response));
-        },
-        error => {
-            console.log(JSON.stringify(error));
-        },
-        () => {
-            stockChart.style.display = 'block';
-            progressBar.style.display = 'none';
-        });
-
-}
-
-function _apiGet(
-    uri,
-    query,
-    handlerThen,
-    handlerCatch,
-    handleFinally) {
-    const url = new URL(uri)
-
-    query.lang = _api.lang;
-    query.region = _api.region;
-
-    url.search = new URLSearchParams(query).toString();
-
-    fetch(url, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": _api.host,
-            "x-rapidapi-key": _api.key
-        }
+  })
+    .then(response => {
+      return response.json();
     })
-        .then(response => {
-            return response.json()
-        })
-        .then(json => {
-            handlerThen(json);
-        })
-        .catch(err => {
-            handlerCatch(err);
-        })
-        .finally(() => {
-            if (handleFinally !== undefined) {
-                handleFinally();
-            }
-        });
-}
-
-function openMenu() {
-    document.getElementById("menu").style.width = "100%";
-}
-
-function closeMenu() {
-    document.getElementById("menu").style.width = "0%";
-}
-
-function addCompany() {
-    const company = {
-        value: document.getElementById("company-selector").value,
-        text: document.getElementById("company-name").innerText
-    }
-    var companies = JSON.parse(localStorage.getItem("company_list"));
-    if (companies === null) {
-        companies = [];
-    }
-    if (companies.find(c => c.value === company.value) === undefined) {
-        companies.push(company);
-    }
-    localStorage.setItem("company_list", JSON.stringify(companies));
-}
-
-function customizeChart() {
-    const company = document.getElementById("company-selector").value;
-    const interval = _intervals.find(x => x.name === document.getElementById("interval").value);
-    const range = _ranges.find(x => x.name === document.getElementById("range").value);
-
-    document.getElementById("interval-data").value = interval.value;
-    document.getElementById("range-data").value = range.value;
-
-    closeMenu();
-
-    drawCompanyChart(company);
-}
-
-function _drawChart(response) {
-
-    var dataSeries = [];
-    response.chart.result[0].timestamp.forEach(row => {
-        dataSeries.push([new Date(row * 1000), 0, 0, 0, 0]);
+    .then(json => {
+      handlerThen(json);
+    })
+    .catch(err => {
+      handlerCatch(err);
+    })
+    .finally(() => {
+      if (handleFinally !== undefined) {
+        handleFinally();
+      }
     });
+};
 
-    // Low
-    var idx = 0;
-    response.chart.result[0].indicators.quote[0].low.forEach(row => {
-        dataSeries[idx][1] = row;
-        idx++;
+openMenu = () => {
+  document.getElementById("menu").style.width = "100%";
+};
+
+closeMenu = () => {
+  document.getElementById("menu").style.width = "0%";
+};
+
+addCompany = () => {
+  const company = document.getElementById("company-selector");
+
+  if (!("symbol" in company.dataset) || !("name" in company.dataset)) {
+    return;
+  }
+  var companies = JSON.parse(localStorage.getItem("company_list"));
+  if (companies === null) {
+    companies = [];
+  }
+  if (companies.find(c => c.value === company.dataset.name) === undefined) {
+    companies.push({
+      symbol: company.dataset.symbol,
+      name: company.dataset.name
     });
-    // Open
-    idx = 0;
-    response.chart.result[0].indicators.quote[0].open.forEach(row => {
-        dataSeries[idx][2] = row;
-        idx++;
-    });
-    // Close
-    idx = 0;
-    response.chart.result[0].indicators.quote[0].close.forEach(row => {
-        dataSeries[idx][3] = row;
-        idx++;
-    });
-    // High
-    idx = 0;
-    response.chart.result[0].indicators.quote[0].high.forEach(row => {
-        dataSeries[idx][4] = row;
-        idx++;
-    });
-    var data = google.visualization.arrayToDataTable(dataSeries, true);
-    /*var options = {
+  }
+  localStorage.setItem("company_list", JSON.stringify(companies));
+};
+
+customizeChart = () => {
+  const company = document.getElementById("company-selector");
+
+  closeMenu();
+
+  if ("symbol" in company.dataset && company.dataset.symbol.length > 0) {
+    drawCompanyChart(company.dataset.symbol);
+  }
+};
+
+_drawChart = response => {
+  var dataSeries = [];
+  response.chart.result[0].timestamp.forEach(row => {
+    dataSeries.push([new Date(row * 1000), 0, 0, 0, 0]);
+  });
+
+  // Low
+  var idx = 0;
+  response.chart.result[0].indicators.quote[0].low.forEach(row => {
+    dataSeries[idx][1] = row;
+    idx++;
+  });
+  // Open
+  idx = 0;
+  response.chart.result[0].indicators.quote[0].open.forEach(row => {
+    dataSeries[idx][2] = row;
+    idx++;
+  });
+  // Close
+  idx = 0;
+  response.chart.result[0].indicators.quote[0].close.forEach(row => {
+    dataSeries[idx][3] = row;
+    idx++;
+  });
+  // High
+  idx = 0;
+  response.chart.result[0].indicators.quote[0].high.forEach(row => {
+    dataSeries[idx][4] = row;
+    idx++;
+  });
+  var data = google.visualization.arrayToDataTable(dataSeries, true);
+  /*var options = {
         title: 'Rate the Day on a Scale of 1 to 10',
         width: 900,
         height: 500,
@@ -293,13 +189,24 @@ function _drawChart(response) {
           minValue: 0
         }
       };*/
-    var options = {
-        legend: 'none'
-    };
+  var options = {
+    legend: "none"
+  };
 
-    if (_stockChart === null) {
-        _stockChart = new google.visualization.CandlestickChart(document.getElementById('stock-chart'));
-    }
+  if (_stockChart === null) {
+    _stockChart = new google.visualization.CandlestickChart(
+      document.getElementById("stock-chart")
+    );
+  }
 
-    _stockChart.draw(data, options);
-}
+  _stockChart.draw(data, options);
+};
+
+showDialogMessage = message => {
+  document.getElementById("dialog-message-text").innerText = message;
+  document.getElementById("dialog-message").style.display = "flex";
+};
+
+closeDialogMessage = () => {
+  document.getElementById("dialog-message").style.display = "none";
+};
